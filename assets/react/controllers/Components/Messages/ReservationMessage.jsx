45013e182416ba4d35456b7f837e1e263e6ef9b2
jugouin <juliegouin@outlook.com>
2024-09-27 17:34:02 +0200

    message

--- assets/react/controllers/Components/Messages/ReservationMessage.jsx
+++ assets/react/controllers/Components/Messages/ReservationMessage.jsx
@@ -1,10 +1,11 @@
 export const ReservationMessage = (formData) => {
     const { name, surname, phone, email, date, privatized, remarks, personNb, saunaType } = formData;
 
-    if(privatized == false){
-        const persons = personNb;
-        const price = persons * 20
-        return price;
+    let price;
+    if (privatized) {
+        price = saunaType === 'petit' ? 40 : 120;
+    } else {
+        price = personNb * 20;
     }
     
     return {
@@ -39,7 +40,7 @@ export const ReservationMessage = (formData) => {
         ${remarks ? `Remarques : ${remarks}` : ''}
 
 
-        Le prix de la réservation est de ${privatized ? `${saunaType === 'petit' ? '40' : '120'}` : `prix qui ne fonctionne pas` } CHF, vous pouvez régler directement sur place en espèce ou par TWINT.
+        Le prix de la réservation est de ${privatized ? `${saunaType === 'petit' ? '40' : '120'}` : `${price}` } CHF, vous pouvez régler directement sur place en espèce ou par TWINT.
 
         Nous vous remercions de vous présenter avec vos linges de bain et 10 minutes avant le début de votre réservation.
 
