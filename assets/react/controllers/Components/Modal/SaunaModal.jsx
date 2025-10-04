5bdf49959caf7de5f67237dcb43722c44fbdeaae
jugouin <juliegouin@outlook.com>
2025-10-04 11:40:36 +0200

    fix btn

--- assets/react/controllers/Components/Modal/SaunaModal.jsx
+++ assets/react/controllers/Components/Modal/SaunaModal.jsx
@@ -15,6 +15,8 @@ const style = {
     p: 4,
 };
 
+const disabled = isDisabled();
+
 const isDisabled = () => {
   const date = new Date()
   const year = date.getFullYear();
@@ -40,7 +42,7 @@ export default function SaunaModal({ open, handleClose, title }) {
         <Button onClick={handleClose} sx={{ color: '#462A1A' }}>
           <a href="/reservation?saunaType=petit">Le petit sauna (4 personnes)</a>
         </Button>
-        <Button onClick={handleClose}  disabled={isDisabled} sx={{ color: '#462A1A' }}>
+        <Button onClick={handleClose}  disabled={disabled} sx={{ color: '#462A1A' }}>
           <a href="/reservation?saunaType=grand">Le grand sauna (10 personnes)</a>
         </Button>
         <Button onClick={handleClose} sx={{ color: '#462A1A', position: 'absolute', top: '10%', left: '80%' }}>
